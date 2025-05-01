c7c732434816d2028df75eb38a128726ed4abb6f
Szymon Mikulski <simmik@poczta.onet.pl>
2025-05-01 10:17:42 +0200

    Add point 5

--- interwencje.py
+++ interwencje.py
@@ -19,7 +19,7 @@ report['percentage_of_signs_of_mental_illness_for_each_race'] = report.apply(lam
 
 most_mental_ill_race = report['percentage_of_signs_of_mental_illness_for_each_race'].idxmax()
 print(report)
-print(f"Największym odsetkiem znamion choroby psychicznej podczas interwencji charakteryzuje się rasa {most_mental_ill_race}")
+print(f"Największym odsetkiem znamion choroby psychicznej podczas interwencji charakteryzuje się rasa {most_mental_ill_race}")   #biała
 
 #4
 #stwórz słownik dni tygodnia
@@ -43,6 +43,48 @@ for i, v in enumerate(report2['id']):
     ax.text(i, v + 10, str(v), ha='center', va='bottom', fontsize=10)
 plt.xticks(rotation=45)  
 plt.show()
+#print(report2)
 
-print(report2)
+#5
+#zaimportuj dane o populacji ze strony www
+path = 'https://simple.wikipedia.org/wiki/List_of_U.S._states_by_population'
+df_population = pd.read_html(path, header=0)[0]
+#wybierz odpowiednie kolumny i wiersze
+report3 = df_population.iloc[:-4, [2,3]]
 
+report3.columns = ['NAZWA STANU', 'LUDNOŚĆ']
+
+#zaimportuj dane o skrótach ze strony www
+path = 'https://en.wikipedia.org/wiki/List_of_U.S._state_and_territory_abbreviations'
+df_abbreviations = pd.read_html(path, header=0)[1].iloc[:, [0,5]].dropna().replace(r'\[.*?\]', '', regex=True)
+#powyższa linia:
+#sczytuje z podanej ścieżki drugą tabelę na stronie
+#wybiera z niej wszystkie wiersze oraz kolumny 0 i 5
+#usuwa wiersze z brakującymi danymi
+#zamienia tekst w nawiasach kwadratowych (za pomocą wyrażenia regularnego) na pusty string
+
+#przygotuj słownik skrótów nazw stanów
+abbreviations = dict(zip(df_abbreviations.iloc[:, 0], df_abbreviations.iloc[:, 1] ))
+#dodaj na drugim miejscu kolumnę z kodem stanu
+report3.insert(1, 'KOD', report3['NAZWA STANU'].map(abbreviations))
+
+#na podstawie oryginalnego df przygotuj słownik liczby interwencji według stanu
+interventions = df.groupby('state')['id'].count().to_dict()
+#dodaj do raportu kolumnę z liczbą interwencji w stanie, brakujące wartości uzupełnij jako 0, zmień typ na int
+report3['INTERWENCJE'] = report3['KOD'].map(interventions).fillna(0).astype(int)
+
+#funkcja do liczenia interwencji na 1000 mieszkańców
+def int_per_1000_inh(row):
+    if row['LUDNOŚĆ'] == 0:
+        result = 0
+    else:
+        result = round(row['INTERWENCJE']*1000 / row['LUDNOŚĆ'], 3)
+    return result
+
+#dodaj do raportu kolumnę z liczbą interwencji na 1000 mieszkańców 
+report3['NA 1000 MIESZK.'] = report3.apply(lambda row: int_per_1000_inh(row), axis=1)
+
+#posortuj na miejscu po liczbie interwencji na 1000 mieszkańców
+report3.sort_values(by='NA 1000 MIESZK.', ascending=False, inplace=True)
+report3.reset_index(drop=True, inplace=True)
+print(report3)
